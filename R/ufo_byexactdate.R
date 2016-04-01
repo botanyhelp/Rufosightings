f03c91aeb4956cefcd5b07b2861cc83236675bad
Tom <botanyhelp@gmail.com>
2016-04-01 16:23:56 +0000

    First commit of R data package ufosightings

---
+++ R/ufo_byexactdate.R
@@ -0,0 +1,10 @@
+#' ufo_byexactdate function
+#'
+#' This function allows you to find UFO sightings on an exact date. 
+#'
+#' @param exactdata  Defaults to 19951011
+#' ufo_byexactdate()
+
+ufo_byexactdate <- function(exactdate=19951011) {
+    ufosightings[ufosightings$reported_at==exactdate,]
+}
