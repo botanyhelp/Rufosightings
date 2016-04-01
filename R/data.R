e2c33645b691c84f2f2541afc81c59245e330ca4
Tom <botanyhelp@gmail.com>
2016-04-01 17:33:21 +0000

    Added everything but now data file and package name are same

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' UFO Sightings
+#'
+#' A dataset containing reports of UFO sightings
+#'
+#' @format A data frame with 55633 rows and 9 variables:
+#' \describe{
+#'   item{sighted_at_year}{sighted_at_year}
+#'   item{sighted_at_month}{sighted_at_month}
+#'   item{sighted_at_day}{sighted_at_day}
+#'   item{reported_at}{reported_at}
+#'   item{location_city}{location_city}
+#'   item{location_state}{location_state}
+#'   item{shape}{shape}
+#'   item{duration}{duration}
+#'   item{description}{description}
+#' }
+#' @source \url{http://shop.oreilly.com/product/0636920018483.do}
+"Rufosightings"
